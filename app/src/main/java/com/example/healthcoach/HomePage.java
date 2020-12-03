f388156ee56ead1cd95acc5251689aa948c5c07b
brian-mulei <brianmulei0@gmail.com>
2020-12-04 02:16:03 +0300

    Step Counting complete

--- app/src/main/java/com/example/healthcoach/HomePage.java
+++ app/src/main/java/com/example/healthcoach/HomePage.java
@@ -1,16 +1,36 @@
 package com.example.healthcoach;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-/*
-import com.sdc19.coolhealthdashboard.ExerciseReporter.ExerciseSummary;
-import com.sdc19.coolhealthdashboard.StepCountReporter.StepCountSummary;
-*/
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
 
-public class HomePage extends AppCompatActivity {
+import java.util.ArrayList;
 
+
+public class HomePage extends AppCompatActivity   {
+
+    SensorManager sensorManager;
+    boolean isRunning =false;
+    TextView steps;
+     double MagnitudePrevious = 0;
+     Integer stepCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +38,65 @@ public class HomePage extends AppCompatActivity {
 
         setTitle("Health Coach");
 
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        steps =findViewById(R.id.steps_title);
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent!= null){
+                    float x_acceleration = sensorEvent.values[0];
+                    float y_acceleration = sensorEvent.values[1];
+                    float z_acceleration = sensorEvent.values[2];
+
+                    double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                    double MagnitudeDelta = Magnitude-MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if (MagnitudeDelta > 8){
+                        stepCount++;
+                    }
+                    steps.setText(stepCount.toString() +"/5000 steps");
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
 
     }
+
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        stepCount = sharedPreferences.getInt("stepCount", 0);
+    }
+
 
 }
--- app/src/main/java/com/example/healthcoach/StepCountReporter.java
+++
@@ -1,7 +0,0 @@
-
-
-package com.example.healthcoach;
-
-public class StepCountReporter {
-
-}
